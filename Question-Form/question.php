073015c8eb753c7f40442be3d27ae0ebe78b8b04
ronakkumarkb3684 <36426320+ronakkumarkb3684@users.noreply.github.com>
2019-11-24 12:15:58 -0500

    Add files via upload

--- Question-Form/question.php
+++ Question-Form/question.php
@@ -1,27 +1,68 @@
 <?php
 
+//session_start();
+//$aEmail = $_SESSION['email'];
+
+require("../pdo.php");
+
+//session_start();
+
+//$email = $_SESSION['email'];
 //Get values from the input
+$userId = filter_input(INPUT_GET, 'userId');
 $QuestionName = filter_input(INPUT_POST, 'Question-name');
 $QuestionBody = filter_input(INPUT_POST, 'Question-body');
 $QuestionSkills = filter_input(INPUT_POST, 'Question-skills');
 
+
 $CheckSkills = explode(',', $QuestionSkills);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $formValid=true;
     if (empty($QuestionName)) {
         $QuestionErr = "Question name is required";
+        $formValid=false;
     } elseif(strlen($QuestionName) < 3) {
         $QuestionErr = "Please make sure the question name is atleast 3 characters";
+        $formValid=false;
     }
     if (empty($QuestionBody)) {
         $BodyErr = "Question body is required";
+        $formValid=false;
     } elseif (strlen($QuestionBody) >= 500){
         $BodyErr ="Please make sure the question body is less than 500 characters";
+        $formValid=false;
     }
     if (empty($QuestionSkills)){
         $skillsErr = "Please enter a skill";
+        $formValid=false;
     } elseif (count($CheckSkills) < 2) {
         $skillsErr = "Please enter 2 or more skills";
+        $formValid=false;
+    }
+    if($formValid==true){
+
+        //sql query
+        $query = 'INSERT INTO questions
+                (ownerid, title, body, skills)
+              VALUES
+                (:ownerid, :title, :body, :skills)';
+
+        // Create PDO Statement
+        $statement = $db->prepare($query);
+
+        //binding the values to sql
+        $statement->bindValue(':ownerid', $userId);
+        $statement->bindValue(':title', $QuestionName);
+        $statement->bindValue(':body', $QuestionBody);
+        $statement->bindValue(':skills', $QuestionSkills);
+
+        // Execute the SQL Query
+        $statement->execute();
+        // Close the database
+        $statement->closeCursor();
+
+        header("Location: ../Question-List/question-list.php?userId=$userId");
     }
 }
 ?>
