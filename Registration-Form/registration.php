c98cc2661e5332e1ad73a6951b3c39554fc816b7
ronakkumarkb3684 <36426320+ronakkumarkb3684@users.noreply.github.com>
2019-11-24 12:20:36 -0500

    Add files via upload

--- Registration-Form/registration.php
+++ Registration-Form/registration.php
@@ -1,7 +1,10 @@
 <?php
 //define variable for the empty values
 //$firstNameErr = $lastNameErr = "";
+require("../pdo.php");
+//session_start();
 
+//$email = $_SESSION['email'];
 //Get values from the input
 $firstName = filter_input(INPUT_POST, 'first-name');
 $lastName = filter_input(INPUT_POST, 'last-name');
@@ -10,24 +13,56 @@ $email = filter_input(INPUT_POST, 'email');
 $password = filter_input(INPUT_POST, 'new-password');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $formValid=true;
     if (empty($firstName)) {
         $firstNameErr = "First Name is required";
+        $formValid=false;
     }
     if (empty($lastName)){
         $lastNameErr = "Last Name is required";
+        $formValid=false;
     }
     if (empty($birthDay)){
         $birthDayErr = "Birth Date is required";
+        $formValid=false;
     }
     if (empty($email)) {
         $emailErr = "EMAIL is required";
+        $formValid=false;
     } elseif ($double_check === false) {
         $emailErr = "not a valid EMAIL";
+        $formValid=false;
     }
     if (empty($password)){
         $passwordErr = "PASSWORD is required";
+        $formValid=false;
     } elseif (strlen($password) <= 8){
         $passwordErr = "please make sure your PASSWORD is at least 8 characters";
+        $formValid=false;
+    }
+    if($formValid==true){
+        //sql query
+        $query = 'INSERT INTO accounts
+                (email, fname, lname, birthday, password)
+              VALUES
+                (:email, :fname, :lname, :birthday, :password)';
+
+        // Create PDO Statement
+        $statement = $db->prepare($query);
+
+        //binding the values to sql
+        $statement->bindValue(':email', $email);
+        $statement->bindValue(':fname', $firstName);
+        $statement->bindValue(':lname', $lastName);
+        $statement->bindValue(':birthday', $birthDay);
+        $statement->bindValue(':password', $password);
+
+        // Execute the SQL Query
+        $statement->execute();
+        // Close the database
+        $statement->closeCursor();
+
+        header('Location: ../Question-List/index.html');
     }
 }
 ?>
