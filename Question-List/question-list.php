51ee6dbbc74ca6a4a25aa9615b53c7445c87f3a6
ronakkumarkb3684 <36426320+ronakkumarkb3684@users.noreply.github.com>
2019-11-26 00:34:48 -0500

    Add files via upload

--- Question-List/question-list.php
+++ Question-List/question-list.php
@@ -3,38 +3,47 @@
 require("../pdo.php");
 require("../functions.php");
 $userId = filter_input(INPUT_GET, 'userId');
+
+session_start();
+$_SESSION['userId'] = $userId;
 ?>
 
 <html>
-   <style>
-       table {
-           border-collapse: collapse;
-           width: auto;
-       }
-
-       th, td {
-           padding: 8px;
-           text-align: left;
-           border-bottom: 1px solid black;
-       }
-       h1{
-           margin: 0;
-           padding: 0 0 20px;
-           text-align: center;
-           font-size: 35px;
-       }
-       .p input[type="submit"]{
-           border: none;
-           outline: none;
-           height: 40px;
-           background: #1c8adb;
-           color: #fff;
-           font-size: 18px;
-           border-radius: 20px;
-       }
-    </style>
     <head>
         <link rel="stylesheet" type="text/css" href="main.css">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+        <style>
+            table {
+                border-collapse: collapse;
+                width: auto;
+                color: #fff;
+                background: rgba(0, 0, 0, 0.5);
+            }
+            th, td {
+                padding: 8px;
+                text-align: left;
+                border-bottom: 1px solid black;
+                font-size: 20px;
+            }
+            h1 {
+                margin: 0;
+                padding: 0 0 20px;
+                text-align: center;
+                font-size: 35px;
+            }
+           button {
+                border: none;
+                outline: none;
+                height: 40px;
+                /*background: #1c8adb;*/
+                color: #fff;
+                font-size: 18px;
+                border-radius: 20px;
+            }
+        </style>
         <title>Display Data</title>
     </head>
     <body>
@@ -42,12 +51,13 @@ $userId = filter_input(INPUT_GET, 'userId');
         $questionHistory = get_questions($userId);
         ?></h1>
 
-    <p><button type="submit"><a href="../Question-Form/index.html">Ask a question</a></button></p>
-        <table align="center">
+    <button type="submit"><a href="../Question-Form/index.html">Ask a question</a></button>
+    <p></p>
+        <table class="table table-bordered">
             <tr>
-                <th>Title</th>
-                <th>body</th>
-                <th>skills</th>
+                <th>TITLE</th>
+                <th>BODY</th>
+                <th>SKILLS</th>
             </tr>
             <?php foreach($questionHistory as $question) : ?>
             <tr>
